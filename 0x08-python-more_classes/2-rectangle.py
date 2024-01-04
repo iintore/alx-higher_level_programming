3949a9970b7c1ef4d1bbd8acd8428107d75c7de8
iintore <tonylewismanzi@gmail.com>
2024-01-04 03:31:37 -0800

    PYTHON CLASSES

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module 2-rectangle
+Contains class Rectangle with private attribute width and height,
+and public area and perimeter methods
+"""
+
+
+class Rectangle:
+    """
+    Defines class rectangle with private attribute width and height
+
+    Args:
+        width (int): width
+        height (int): height
+
+    Functions:
+        __init__(self, width, height)
+
